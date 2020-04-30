be1a10fd5fd7d992c9f92e90f99a27c17cf21fe5
_ToOOom <63470060+Yellow-Submarine215@users.noreply.github.com>
2020-04-30 10:52:05 +0200

    add necessary files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name = 'py-update',
+      version = '0.0.1',
+      author = '_ToOOom',
+      author_email = 'yellowsubmarine.dev@gmail;com',
+      keywords = 'update github checker update-checker'
+      url = 'https://yellow-submarine215.github.io/py-update/'
+      classifiers = ['Topic :: Software Development',
+                     'Natural Language :: English',
+                     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)']
+      install_requires = ['requests>=2.23.0']
+      
+      )
+
